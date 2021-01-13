b0dd3130f4ef8b0bd5aef1e7cf19217a4d18aa8b
Aliaksandr Drankou <adrankou@protonmail.com>
2021-01-13 15:20:39 +0100

    initial commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Aliaksandr Drankou on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
